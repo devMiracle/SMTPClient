f9809ebfd61852d185e49050bb282e0a1b9b83f5
Miracle <rjcvjchzljv9@gmail.com>
2020-03-18 00:47:06 +0200

    done

--- SMTPClient/Program.cs
+++ SMTPClient/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,39 @@ namespace SMTPClient
     {
         static void Main(string[] args)
         {
+            SendMessage("r-9@gmail.com", "Hello World!");
+        }
+        public static void SendMessage(string _subject, string _message)
+        {
+            SmtpClient _smtp = new SmtpClient("smtp.gmail.com", 587);
+
+            _smtp.Credentials = new NetworkCredential("username", "password");
+
+            _smtp.EnableSsl = true;
+
+            MailMessage _mail = new MailMessage();
+
+            _mail.From = new MailAddress("mail");
+
+            _mail.To.Add(new MailAddress("mail"));
+
+            _mail.SubjectEncoding = Encoding.UTF8;
+
+            _mail.BodyEncoding = Encoding.UTF8;
+
+            _mail.Subject = _subject;
+
+            _mail.Body = _message;
+
+            try
+            {
+                _smtp.Send(_mail);
+                Console.WriteLine("Done!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
